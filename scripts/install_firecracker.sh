ed34da2aedcfbc1bc4ccd0979222041b60324d38
kallebysantos <kalleby_santos@hotmail.com>
2025-11-15 23:35:40 +0000

    core: initial commit - install firecracker script

---
+++ scripts/install_firecracker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+export $(grep -v '^#' $SCRIPTPATH/../.env | xargs)
+
+ARCH=aarch64
+FIRECRACKER_VERSION=v1.13.1
+DONWLOAD_FILE=
+
+wget -qO- https://github.com/firecracker-microvm/firecracker/releases/download/$FIRECRACKER_VERSION/firecracker-${FIRECRACKER_VERSION}-${ARCH}.tgz | tar zxv
+
+mkdir -p ./temp
+mv "release-${FIRECRACKER_VERSION}-${ARCH}" "./temp/${DONWLOAD_FILE}"
