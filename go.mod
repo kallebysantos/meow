f073acc18ac906eeecb221a14ec25303226678a6
kallebysantos <kalleby_santos@hotmail.com>
2025-11-23 03:06:35 +0000

    feat: creating initial go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kallebysantos/meow
+
+go 1.25.4
---
+++ src/meow/cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kallebysantos/meow/src/meow/pkg/example"
+)
+
+func main() {
+	fmt.Println("Starting project...")
+	example.Hello()
+}
